8336c584c577d4e39712b6853a18bccdda2b9d18
Valentin Tkachev <val.sfinx@gmail.com>
2017-08-23 17:30:13 +0300

    appkernel_for_heroku

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new Symfony\Bundle\DebugBundle\DebugBundle(),
+            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
